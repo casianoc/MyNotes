5d0b9e2acead71caea9778dc601dfd8d6c9cb363
Cameron Casiano <CasianoC@chc.edu>
2019-11-19 09:34:09 -0500

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Casiano, Cameron Joseph on 11/19/19.
+//  Copyright © 2019 Casiano, Cameron Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
